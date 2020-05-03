a36d8e42d2321b318594b6be21f849d636968c09
EmetsLV <50299843+EmetsLV@users.noreply.github.com>
2020-05-03 21:42:40 +0300

    Add files via upload

---
+++ SharedComponents/ParkTerritories.BusinessLogic/DomainObjects/ParksInfrastructure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkTerritories.DomainObjects
+{
+        public enum Waters { yes, no }
+
+        public enum PlayGrounds { yes, no }
+
+        public enum SportsGrounds { yes, no }    
+}
---
+++ SharedComponents/ParkTerritories.BusinessLogic/DomainObjects/Ports/IParkRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ParkTerritories.DomainObjects.Ports
+{
+    public interface IReadOnlyRouteRepository
+    {
+        Task<Park> GetPark(long id);
+
+        Task<IEnumerable<Park>> GetAllParks();
+
+        Task<IEnumerable<Park>> QueryParks(ICriteria<Park> criteria);
+
+    }
+
+    public interface IParkRepository
+    {
+        Task AddPark(Park park);
+
+        Task RemovePark(Park park);
+
+        Task UpdatePark(Park park);
+    }
+}
