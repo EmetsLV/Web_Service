83a1d1271fe0a4f852018d993eb7c34fb5d13703
EmetsLV <50299843+EmetsLV@users.noreply.github.com>
2020-05-05 19:07:26 +0300

    Add files via upload

---
+++ ParkTerritories.RouteListQuery/ApplicationServices/GetParkListUseCase/GetParkListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using ParkTerritories.DomainObjects;
+using ParkTerritories.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkTerritories.ApplicationServices.GetParkListUseCase
+{
+    public class GetParkListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<Park> Parks { get; }
+
+        public GetParkListUseCaseResponse(IEnumerable<Park> parks) => Parks = parks;
+    }
+}
