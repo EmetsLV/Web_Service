2321d9ab77b9e115222d0bb6226f54a1b3f4ff7c
EmetsLV <50299843+EmetsLV@users.noreply.github.com>
2020-05-10 22:09:37 +0300

    Add files via upload

---
+++ ParkTerritories.WebService/DomainObjects/Park.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkTerritories.DomainObjects
+{
+    public class Park : DomainObject
+    {
+        public string Location { get; set; }
+
+        public string Name { get; set; }
+
+        public string Water { get; set; }
+
+        public PlayGrounds PlayGround { get; set; }
+
+        public SportsGrounds SportsGround { get; set; }
+    }
+}
